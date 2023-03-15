6ce32933cf957e9235d7c051e3bf259e5f32f411
liuyao <liuyao@dustess.com>
2023-03-15 15:50:18 +0800

    feat: goweb-14

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	// http.HandleFunc("/", funcs.IndexHandler)
 	// http.HandleFunc("/chat-login", funcs.IndexHandler)
 	http.HandleFunc("/", funcs.ChatDemoHandler)
